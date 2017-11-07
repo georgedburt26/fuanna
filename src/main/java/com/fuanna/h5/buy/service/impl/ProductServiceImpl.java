d3d5a36ebd3fed0dd4f703e9487f8ea0074ff0f9
Administrator <Administrator@192.168.1.82>
2017-11-07 17:43:49 +0800

    update

--- src/main/java/com/fuanna/h5/buy/service/impl/ProductServiceImpl.java
+++ src/main/java/com/fuanna/h5/buy/service/impl/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.fuanna.h5.buy.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,11 @@ import com.fuanna.h5.buy.model.Category;
 import com.fuanna.h5.buy.service.ProductService;
 
 @Component
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
 	@Autowired
 	ProductMapper productMapper;
-	
+
 	@Override
 	public List<Category> listCategories() {
 		return productMapper.listCategories();
@@ -23,7 +24,11 @@ public class ProductServiceImpl implements ProductService{
 
 	@Override
 	public List<Map<String, String>> listProduct(List<Long> ids) {
-		return productMapper.listProduct(ids);
+		List<Map<String, String>> list = productMapper.listProduct(ids);
+		for (Map<String, String> map : list) {
+			formatProductName(map);
+		}
+		return list;
 	}
 
 	@Override
@@ -31,4 +36,13 @@ public class ProductServiceImpl implements ProductService{
 		return productMapper.countProductSku();
 	}
 
+	private Map<String, String> formatProductName(Map<String, String> map) {
+		if (!map.isEmpty() && StringUtils.isNotBlank(map.get("name"))) {
+			String name = map.get("name");
+			if (name.length() > 13) {
+				map.put("name", name.substring(0, name.length() - 1) + "...");
+			}
+		}
+		return map;
+	}
 }
