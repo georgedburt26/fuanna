3c064dc4cf3402ce4b4c414320bf44b595c8e063
Administrator <Administrator@192.168.1.82>
2017-11-09 16:18:09 +0800

    MD5更新

---
+++ src/main/java/com/fuanna/h5/buy/util/MD5.java
@@ -0,0 +1,34 @@
+package com.fuanna.h5.buy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+
+    public static void main(String[] args) {
+        MD5("sunzn");
+    }
+
+    public static String MD5(String str) {
+        String result = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            result = buf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e);
+        }
+        return result;
+    }
+}
